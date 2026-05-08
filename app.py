fad16d37ee8a1fbcd113a66a1ee431d8443cffae
islam <islammahdy89@gmail.com>
2026-05-09 00:27:27 +0300

    Add exercise recommendation API

---
+++ app.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+# إنشاء التطبيق
+app = FastAPI()
+
+# =========================
+# PUBLIC CORS
+# =========================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# تحميل الموديل
+model = joblib.load("exercise_pipeline.pkl")
+
+
+# شكل البيانات القادمة
+class ExerciseData(BaseModel):
+    Sex: str
+    Age: int
+    Height: float
+    Weight: float
+    Hypertension: str
+    Diabetes: str
+    BMI: float
+    Level: str
+    Fitness_Goal: str
+    Fitness_Type: str
+    Equipment: str
+
+
+# Home Route
+@app.get("/")
+def home():
+    return {
+        "message": "Exercise Recommendation API Running Successfully"
+    }
+
+
+# Predict Route
+@app.post("/predict")
+def predict(data: ExerciseData):
+
+    test_df = pd.DataFrame([{
+        "Sex": data.Sex,
+        "Age": data.Age,
+        "Height": data.Height,
+        "Weight": data.Weight,
+        "Hypertension": data.Hypertension,
+        "Diabetes": data.Diabetes,
+        "BMI": data.BMI,
+        "Level": data.Level,
+        "Fitness Goal": data.Fitness_Goal,
+        "Fitness Type": data.Fitness_Type,
+        "Equipment": data.Equipment
+    }])
+
+    prediction = model.predict(test_df)
+
+    return {
+        "recommended_exercise": str(prediction[0])
+    }
